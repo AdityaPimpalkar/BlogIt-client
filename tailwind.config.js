186f2b4e6f73b255519263b1d582968d941d6935
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-04-17 23:04:44 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      teal: "#417D7A",
+      white: "#FFF",
+      black: "#000",
+      peach: "#EDE6DB",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
