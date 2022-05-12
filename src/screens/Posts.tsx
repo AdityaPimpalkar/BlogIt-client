7fa908af60b07d99c128684ae9b2627fee22d830
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-05-12 20:47:48 +0530

    navbar component

--- src/screens/Posts.tsx
+++ src/screens/Posts.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PostsCard from "../components/PostsCard";
+import Navbar from "../components/Navbar";
 import { BookmarkIcon, HeartIcon, ChatIcon } from "@heroicons/react/outline";
 
 const Posts = () => {
@@ -18,50 +19,7 @@ const Posts = () => {
   ]);
   return (
     <div className="overflow-x-hidden bg-gray-100">
-      <nav className="px-6 py-4 bg-white shadow">
-        <div className="container flex flex-col mx-auto md:flex-row md:items-center md:justify-between">
-          <div className="flex items-center justify-between">
-            <div>
-              <a
-                href="#"
-                className="text-xl font-bold text-gray-800 md:text-2xl"
-              >
-                Brand
-              </a>
-            </div>
-            <div>
-              <button
-                type="button"
-                className="block text-gray-800 hover:text-gray-600 focus:text-gray-600 focus:outline-none md:hidden"
-              >
-                <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                  <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className="flex-col hidden md:flex md:flex-row md:-mx-4">
-            <a
-              href="#"
-              className="my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className="my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
-            >
-              Blog
-            </a>
-            <a
-              href="#"
-              className="my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
-            >
-              About us
-            </a>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
       <div className="px-6 py-8">
         <div className="container flex justify-between mx-auto">
